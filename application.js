b523892ec592dd0e4823fe356738d7cd2bace815
Vincenzo Acinapura <vin.aci@gmail.com>
2015-03-29 19:18:58 +0200

    Added panTo

--- application.js
+++ application.js
@@ -1,12 +1,18 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoibGlxdWlkMTk4MiIsImEiOiJCdmxWQkZNIn0.itcodaqRcLopL_0WP5Rjww';
 
 var map = L.mapbox.map('my-map', 'examples.map-i86nkdio');
-map.setView([42.5, 13], 8);
+// map.setView([42.5, 13], 8);
 
 $.getJSON('data.json', function(data) {
+  map.setView(data['markers'][0]['coords'], 8);
+
   $(data['markers']).each(function(index, markerData) {
     var marker = L.marker(markerData['coords']);
     marker.bindPopup('<b>' + markerData['title'] + '</b>');
     marker.addTo(map);
+
+    marker.on('click', function(e) {
+      map.panTo(e.latlng);
+    });
   });
 });
