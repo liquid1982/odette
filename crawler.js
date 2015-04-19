197bcb0ddc6638fd174f9aaa88f83595f6a4f7aa
Vincenzo Acinapura <vin.aci@gmail.com>
2015-04-19 16:32:10 +0200

    Added crawler.js

---
+++ crawler.js
@@ -0,0 +1,8 @@
+var request = require('request');
+var cheerio = require('cheerio');
+
+var dataUrl = "http://www.milanodabere.it/milano/teatri";
+
+request(dataUrl, function(error, response, body) {
+
+});
