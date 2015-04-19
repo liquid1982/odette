689431b5a27b6501249c989729b048b77e907e0d
Vincenzo Acinapura <vin.aci@gmail.com>
2015-04-19 17:38:16 +0200

    Work in progress on crawler.js

--- crawler.js
+++ crawler.js
@@ -1,8 +1,33 @@
 var request = require('request');
 var cheerio = require('cheerio');
-
 var dataUrl = "http://www.milanodabere.it/milano/teatri";
 
 request(dataUrl, function(error, response, body) {
+  var $ = cheerio.load(body);
+  var elements = $('#mdb_lista > article');
+  var list = [];
 
+  // "header > div.media-body > aside > div > a:nth-child(2)"
+
+  elements.each(function(index, element) {
+    var obj = {};
+    var addressLink = "";
+
+    obj.title = $('header h1 > a', element).text();
+
+    addressLink = $('header > div.media-body > aside > div > a:nth-child(2)', element).attr('href');
+
+    var tokens = addressLink.split('?');
+
+    tokens = tokens[1].split('&');
+
+    obj.coords = [
+      parseFloat(tokens[0].split('=')[1]),
+      parseFloat(tokens[1].split('=')[1])
+    ];
+
+    list.push(obj);
+  });
+
+  console.log(list);
 });
