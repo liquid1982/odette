84b037c7903caee7302d9b9edb3e110752323988
Vincenzo Acinapura <vin.aci@gmail.com>
2015-04-19 22:28:57 +0200

    Some experiments with async crawler

--- crawler.js
+++ crawler.js
@@ -1,33 +1,46 @@
+var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
-var dataUrl = "http://www.milanodabere.it/milano/teatri";
+var dataUrl = 'http://www.milanodabere.it/milano/teatri';
+var venues = [];
+var baseUrl = 'http://www.milanodabere.it';
+var paths = [
+  '/milano/teatri',
+  '/milano/teatri/2',
+  '/milano/teatri/3',
+  '/milano/teatri/4',
+  '/milano/teatri/5'
+];
 
-request(dataUrl, function(error, response, body) {
-  var $ = cheerio.load(body);
-  var elements = $('#mdb_lista > article');
-  var list = [];
+paths.forEach(function(path, index) {
+  request(baseUrl + path, function(error, response, body) {
+    var $ = cheerio.load(body);
+    var elements = $('#mdb_lista > article');
 
-  // "header > div.media-body > aside > div > a:nth-child(2)"
+    elements.each(function(index, element) {
+      var venue = {};
+      var addressLink, tokens, latitude, longitude;
 
-  elements.each(function(index, element) {
-    var obj = {};
-    var addressLink = "";
+      addressLink = $('header > div.media-body > aside > div > a:nth-child(2)', element).attr('href');
+      tokens = addressLink.split('?');
+      tokens = tokens[1].split('&');
+      latitude = tokens[0].split('=')[1];
+      longitude = tokens[1].split('=')[1];
 
-    obj.title = $('header h1 > a', element).text();
+      venue.name = $('header h1 > a', element).text();
+      venue.coords = [parseFloat(latitude), parseFloat(longitude)];
 
-    addressLink = $('header > div.media-body > aside > div > a:nth-child(2)', element).attr('href');
+      console.log('Venue data:', venue);
 
-    var tokens = addressLink.split('?');
-
-    tokens = tokens[1].split('&');
-
-    obj.coords = [
-      parseFloat(tokens[0].split('=')[1]),
-      parseFloat(tokens[1].split('=')[1])
-    ];
-
-    list.push(obj);
+      venues.push(venue);
+    });
   });
+});
 
-  console.log(list);
+fs.writeFile('venues.json', JSON.stringify(venues, null, 2), function(error) {
+  if (!error) {
+    console.log('All venues have been written to venues.json file!');
+  } else {
+    console.log('Error writing venues.json!', error);
+  }
 });
