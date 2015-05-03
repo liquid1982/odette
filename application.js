b6a020c33709d2c5ebbbb183dc9bec3a2d20d012
Vincenzo Acinapura <vin.aci@gmail.com>
2015-05-03 15:48:20 +0200

    Small changes

--- application.js
+++ application.js
@@ -1,31 +1,36 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoibGlxdWlkMTk4MiIsImEiOiJCdmxWQkZNIn0.itcodaqRcLopL_0WP5Rjww';
 
 var map = L.mapbox.map('map', 'examples.map-i86nkdio');
-// map.setView([42.5, 13], 8);
+map.setView([45.464262, 9.190802], 14);
+
+var filterVenues = function(venues, options) {
+  venues.filter(function(venue) {
+  }, options);
+}
 
 $.getJSON('venues.json', function(data) {
-  map.setView(data['venues'][0]['coords'], 13);
-
   data['venues'].forEach(function(markerData, index) {
     var marker = L.marker(markerData['coords']);
 
     marker.bindPopup('<b>' + markerData['name'] + '</b>');
+    marker['data'] = markerData;
     marker.addTo(map);
 
     marker.on('click', function(e) {
       map.panTo(e.latlng);
+      console.log('Marker data:', marker);
     });
   });
 });
 
 $("#date-range").ionRangeSlider({
   type: 'double',
+  force_edges: true,
+  drag_interval: true,
   min: +moment().subtract(2, 'months').format('X'),
   max: +moment().add(6, 'months').format('X'),
   from: +moment().format('X'),
   to: +moment().add(3, 'months').format('X'),
-  force_edges: true,
-  drag_interval: true,
   prettify: function(number) {
     return moment(number, 'X').format('LL');
   }
