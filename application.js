8f5c45170d34be0883e711fb5cc8e6fd1f8902e3
Vincenzo Acinapura <vin.aci@gmail.com>
2015-05-03 20:14:25 +0200

    More efficient way to show / hide markers

--- application.js
+++ application.js
@@ -1,65 +1,64 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoibGlxdWlkMTk4MiIsImEiOiJCdmxWQkZNIn0.itcodaqRcLopL_0WP5Rjww';
 
-var markers = [];
-var markerInstances = [];
-var startDate = +moment().format('X');
-var endDate = +moment().add(3, 'months').format('X');
+var map = L.mapbox.map('map', 'examples.map-i86nkdio');
+var venueMarkers = [];
+var fromDate = +moment().format('X');
+var toDate = +moment().add(3, 'months').format('X');
 var minDate = +moment().subtract(2, 'months').format('X');
 var maxDate = +moment().add(6, 'months').format('X');
-var map = L.mapbox.map('map', 'examples.map-i86nkdio');
 
 map.setView([45.464262, 9.190802], 14);
 
-var hasEvents = function(markerData) {
-  return markerData['events'].length && markerData['events'].some(function(event) {
-    var start = new Date(event['startsAt']).getTime() > startDate * 1000;
-    var end = new Date(event['endsAt']).getTime() < endDate * 1000;
+var hasEvents = function(info) {
+  return info['events'].length && info['events'].some(function(event) {
+    var start = new Date(event['startsAt']).getTime() > fromDate * 1000;
+    var end = new Date(event['endsAt']).getTime() < toDate * 1000;
 
     return start && end;
   });
 }
 
-var showMarkers = function() {
-  markerInstances.forEach(function(marker) {
-    map.removeLayer(marker);
-  });
-
-  markers.forEach(function(markerData) {
-    if (hasEvents(markerData)) {
-      var marker = L.marker(markerData['coords']);
-
-      marker.bindPopup('<b>' + markerData['name'] + '</b>');
+var showVenues = function() {
+  venueMarkers.forEach(function(marker) {
+    if (hasEvents(marker.data)) {
       marker.addTo(map);
-
-      marker.on('click', function(e) {
-        map.panTo(e.latlng);
-        console.log('Marker data:', marker);
-      });
-
-      markerInstances.push(marker);
     } else {
-      console.log('This markers has no events in the specified time range.', marker);
+      map.removeLayer(marker);
     }
   });
 }
 
+var setupMarkers = function(payloads) {
+  var markers = [];
+
+  payloads.forEach(function(payload) {
+    var marker = L.marker(payload['coords']);
+    marker.data = payload;
+    marker.bindPopup('<b>' + payload['name'] + '</b>');
+    marker.on('click', function() { map.panTo(e.latlng); });
+    markers.push(marker);
+  });
+
+  return markers;
+}
+
 $.getJSON('venues.json', function(data) {
-  markers = data['venues'];
-  showMarkers();
+  venueMarkers = setupMarkers(data['venues']);
+  showVenues();
 });
 
-var rangeSlider = $("#date-range").ionRangeSlider({
+var rangeSlider = $('#date-range').ionRangeSlider({
   type: 'double',
   force_edges: true,
   drag_interval: true,
   min: minDate,
   max: maxDate,
-  from: startDate,
-  to: endDate,
-  onChange: function(data) {
-    startDate = data.from;
-    endDate = data.to;
-    showMarkers();
+  from: fromDate,
+  to: toDate,
+  onChange: function(options) {
+    fromDate = options.from;
+    toDate = options.to;
+    showVenues();
   },
   prettify: function(number) {
     return moment(number, 'X').format('LL');
